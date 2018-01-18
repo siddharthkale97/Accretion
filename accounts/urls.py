b69c6d3e8c2339bf92c5edd7ee55fad569e816bf
Siddharth Kale <siddharthkale97@gmail.com>
2018-01-19 01:44:52 +0530

    Edit logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from . import views
 
 urlpatterns = [
     path('home/', views.home),
     path('login/', login, {'template_name': 'accounts/login.html'}),
+    path('logout/', logout, {'template_name': 'accounts/logout.html'}),
 ]
