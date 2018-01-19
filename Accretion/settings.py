3730f06a067ba70f7f4650934465b25b65a73798
Siddharth Kale <siddharthkale97@gmail.com>
2018-01-19 14:39:47 +0530

    prevent anonymous user redirect account home

--- Accretion/settings.py
+++ Accretion/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'homepage',
+    'homepage',
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/accounts/home/'
--- Accretion/urls.py
+++ Accretion/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('homepage.urls')),
+    path('home/', include('homepage.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
