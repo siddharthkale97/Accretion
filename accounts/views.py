a33f4d2a274e89a8c3cd619353871b63366e9b09
Siddharth Kale <siddharthkale97@gmail.com>
2018-02-04 01:25:34 +0530

    Registraion form and login css added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from accounts.forms import RegistrationForm
 # Create your views here.
 
+def register(request):
+     if request.user.is_authenticated:
+         return redirect('/accounts/home')
+     else:
+         if request.method == 'POST':
+             form = RegistrationForm(request.POST)
+             if form.is_valid():
+                 form.save()
+                 return redirect('/accounts/home/')
+         else:
+             form = RegistrationForm()
+
+             args = {'form': form}
+             return render(request, 'accounts/reg_form.html', args)
 
 def home(request):
     if request.user.is_authenticated:
