f3a77fb557947fd8159b948983a7db3bb69d2d34
harsh42774 <tamakuwalaharsh42774@outlook.com>
2018-02-24 04:08:43 +0530

    Created post using GET method

--- homepage/views.py
+++ homepage/views.py
@@ -1,4 +1,11 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from homepage.forms import HomeForm
+
 
 class HomeView(TemplateView):
 	template_name = 'homepage/home.html'
+
+	def get(self, request):
+		form = HomeForm()
+		return render(request, self.template_name, {'form': form})
