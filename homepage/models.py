0278059c6b6a9bf5dc407afc24b1bacd9d854d4b
harsh42774 <tamakuwalaharsh42774@outlook.com>
2018-02-25 15:32:52 +0530

    Displaying posts on homescreen by date and adding a new updated field for saving the date and time the post was updated

--- homepage/models.py
+++ homepage/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     post = models.CharField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
